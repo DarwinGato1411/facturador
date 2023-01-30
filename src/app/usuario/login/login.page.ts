373c17bc84223e0b2bde63d1e60ec18207e7c158
NanoDan29 <jowell29jm@gmail.com>
2023-01-30 15:12:46 -0500

    arreglo en vistas

--- src/app/usuario/login/login.page.ts
+++ src/app/usuario/login/login.page.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ContentChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AlertController, LoadingController } from '@ionic/angular';
 import { ConeccionapiService } from 'src/app/coneccionapi.service';
 import Swal from 'sweetalert2';
+import { IonInput } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -12,8 +13,8 @@ import Swal from 'sweetalert2';
 })
 export class LoginPage implements OnInit {
 
-  
-
+ 
+  click = false;
   loginForm = new FormGroup({
     usuarionombre: new FormControl(),
     password: new FormControl(),
@@ -26,29 +27,43 @@ export class LoginPage implements OnInit {
     private loadingController: LoadingController,
     private router: Router) {
 
-   
+
   }
+
+  @ContentChild(IonInput) input: IonInput;
 
   ngOnInit() {
   }
 
+  toggleShow() {
+    let password = document.getElementById('password');
+    console.log(this.click)
+    if (!this.click) {
+      password.setAttribute("type",'text')
+      this.click = true
+    } else if (this.click) {
+      password.setAttribute("type",'password');
+      this.click = false
+    }
+  }
+
   async autentificar() {
-    let ipServidor=localStorage.getItem('ipservidor')
-    let puerto=localStorage.getItem('puerto')
-  
-    if(ipServidor && puerto){
+    let ipServidor = localStorage.getItem('ipservidor')
+    let puerto = localStorage.getItem('puerto')
+
+    if (ipServidor && puerto) {
       this.cnx.login(this.loginForm.value.usuarionombre, this.loginForm.value.password)
-    }else{
+    } else {
       Swal.fire({
         icon: 'warning',
         title: 'Error!',
-        text:'Por favor ingrese la configuración del servidor',
+        text: 'Por favor ingrese la configuración del servidor',
         showConfirmButton: false,
-        timer:2000,
+        timer: 2000,
       })
     }
 
-   
+
 
   }
   async presentAlert(mensaje) {
