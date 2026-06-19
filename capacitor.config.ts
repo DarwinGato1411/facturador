cb0729227735d2d67d4ebd7b5480d20413fc4d52
DarwinGato1411 <darwinvinicio14@gmail.com>
2026-06-19 01:17:11 -0500

    Darwin estable

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,20 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.deckxelfacturacion.app',
-  appName: 'DECKXEL',
+  appId: 'com.example.app',
+  appName: 'Facturador',
   webDir: 'www',
-  bundledWebRuntime: false
+  cordova: {
+    preferences: {
+      ScrollEnabled: 'false',
+      BackupWebStorage: 'none',
+      SplashMaintainAspectRatio: 'true',
+      FadeSplashScreenDuration: '300',
+      SplashShowOnlyFirstTime: 'false',
+      SplashScreen: 'screen',
+      SplashScreenDelay: '3000'
+    }
+  }
 };
 
 export default config;
